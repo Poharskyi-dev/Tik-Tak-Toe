3cdf4933bc13fe3a9c97ba37e50ba6d801014c0a
Poharskyi <avpog.box@gmail.com>
2024-10-28 20:34:51 +0100

    XO drawing + Players init

--- src/main/java/hw/tiktaktoe/poharskyi/App.java
+++ src/main/java/hw/tiktaktoe/poharskyi/App.java
@@ -1,83 +1,77 @@
 package hw.tiktaktoe.poharskyi;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        // create array
-        String[][] board = new String[5][5]; // FOR SCAN
+        Scanner input = new Scanner(System.in);
 
-        // empty cell fill
-        for (int rowInd = 0; rowInd < board.length; rowInd++) {
-            for (int colInd = 0; colInd < board[rowInd].length; colInd++) {
-                board[rowInd][colInd] = " ";
-            }
+        // Grid size input
+        System.out.print("Please enter row quantity of the grid: ");
+        int rowCount = input.nextInt();
+        input.nextLine();
+        System.out.println();
+        System.out.print("Please enter columns quantity of the grid: ");
+        int colCount = input.nextInt();
+        input.nextLine();
+        System.out.println();
+
+        // Player names input
+        System.out.print("Please enter Player 1 name: ");
+        String playerName01 = input.nextLine();
+        System.out.println();
+
+        // Type char var init
+        char playerType01;
+        char playerType02;
+
+        // Name and Type questions for P1
+        do {
+            System.out.print("Please enter Player 1 symbol (X or Y): ");
+            playerType01 = input.nextLine().toUpperCase().charAt(0);
+        } while (Player.isNotValidSymbol(playerType01));
+
+        if (playerType01 == 'X') {
+            playerType02 = 'O';
+
+        } else {
+            playerType02 = 'X';
         }
 
-        // temp for test
-        board[0][0] = "00";
-        board[0][1] = "01";
-        board[0][2] = "02";
-        board[1][0] = "10";
-        board[1][1] = "11";
-        board[1][2] = "12";
-        board[2][0] = "20";
-        board[2][1] = "21";
-        board[2][2] = "22";
+        // Name and Type questions for P2
+        System.out.print("Please enter Player 2 name: ");
+        String playerName02 = input.nextLine();
+        System.out.println();
 
-        // MESH CREATING
-        // row loop
-        for (int rowInd = 0; rowInd < board.length; rowInd++) {
-            // 1st row inside line
-            // if not last - draw!
-            for (int colInd = 0; colInd < board[rowInd].length; colInd++) {
-                if (colInd < board[colInd].length - 1) {
-                    System.out.print("    #");
-                }
-            }
-            // new line after draw
-            System.out.println();
-            // 2nd row inside line
-            // if not last - draw!
-            for (int colInd = 0; colInd < board[rowInd].length; colInd++) {
-                if (colInd < board[colInd].length - 1) {
-                    System.out.print("    #");
-                }
-            }
-            // new line after draw
-            System.out.println();
-            // 3rd row inside line
-            // if not last - draw!
-            if (rowInd < board[rowInd].length - 1) {
-                for (int colInd = 0; colInd < board[rowInd].length; colInd++) {
-                    System.out.print("#####");
-                }
-            }
-            // new line after draw
-            System.out.println();
-        }
+        // Char to enum
+        PlayerType playerType01Enum = PlayerType.valueOf(String.valueOf(playerType01));
+        PlayerType playerType02Enum = PlayerType.valueOf(String.valueOf(playerType02));
+
+        // Player objects creating
+        Player player01 = new Player(playerName01, playerType01Enum);
+        Player player02 = new Player(playerName02, playerType02Enum);
+
+        // Objects inspection
+        System.out.println(player01.toString());
+        System.out.println(player02.toString());
+        input.close();
+
+        // Board drawing
+        Board board = new Board(rowCount, colCount);
+
+        board.initBoard();
+
+        board.setO(1, 1);
+        board.setX(2, 2);
+
+        board.boardDrawing();
+
+
     }
+
+
+
+
+
+
 }
-// ◜◝
-// ◟◞
-
-// \/
-// /\
-
-
-//    #     #
-//  X #     #
-//###############
-//    #     #
-//    #     #
-//###############
-//    #     #
-//    #     #
-
-
-
-
-
-
